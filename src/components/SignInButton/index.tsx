c004a275f3278a427e90775b268f2c86a680d486
Luiza Campos <luiza.almcampos@gmail.com>
2021-03-26 11:10:10 -0300

    commit

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,31 @@
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
+
+import styles from './styles.module.scss'
+
+export function SignInButton() {
+    const [session] = useSession()
+
+    console.log(session)
+
+    return session ? (
+        <button className={styles.signInButton} 
+            type="button"
+            onClick={() => signOut()}
+        >
+            <FaGithub color="#04d361" />
+            {session.user.name}
+            <FiX color="#737380" className={styles.closeIcon} />
+        </button>
+    ) : (
+        <button 
+            className={styles.signInButton} 
+            type="button"
+            onClick={() => signIn('github')}
+        >
+            <FaGithub color="#eba417" />
+            Sign in with Github
+        </button>
+    )
+}
---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.scss'
+
+interface SubscribeButtonProps {
+    priceId: string
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    return (
+        <button type="button" className={styles.subscribeButton}>
+            Subscribe now
+        </button>
+    )
+}
