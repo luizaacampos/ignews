566139357892cc4044789d008702d39cd6c359c3
Luiza Campos <luiza.almcampos@gmail.com>
2021-03-29 13:58:10 -0300

    checkout stripe adicionado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home({ product }: HomeProps) {
       
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
-          <span>👏 Hey, welcome</span>
+          <span>👋🏻 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
