03f05f2826730c46148e22bf81d227b82f7eef70
Luiza Campos <luiza.almcampos@gmail.com>
2021-03-29 09:41:16 -0300

    faunaDB adicionado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,11 @@
+import {query as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
+import { session } from 'next-auth/client'
 import Providers from 'next-auth/providers'
 
+import { fauna } from '../../../services/fauna'
+ 
 export default NextAuth({
     providers: [
         Providers.GitHub({
@@ -9,4 +14,39 @@ export default NextAuth({
         scope: 'read:user'
     }),
     ],
+    callbacks: {
+        async signIn(user, account, profile) {
+            const { email } = user
+
+            try {
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            { data: { email } }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(user.email)
+                            )
+                        )
+                    )
+                )
+
+                return true
+            } catch {
+                return false
+            }
+
+        }
+    }
 })
