da2edc6662f45e77d04a407b24bbef4fe76536c6
Luiza Campos <luiza.almcampos@gmail.com>
2021-03-30 14:53:42 -0300

    configurando Prismic CMS, listando posts, arrumando navegação com asPath

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,20 @@
 import { SignInButton } from '../SignInButton'
+import { ActiveLink } from '../ActiveLink'
 import styles from './styles.module.scss'
 
 export function Header() {
+
     return (
         <header className={styles.headerContainer}> 
             <div className={styles.headerContent}>
               <img src="/images/logo.svg" alt="ig.news" />
               <nav>
-                  <a className={styles.active}>Home</a>
-                  <a>Posts</a>
+                  <ActiveLink activeClassName={styles.active} href="/">
+                    <a>Home</a>
+                  </ActiveLink>
+                  <ActiveLink activeClassName={styles.active} href="/posts">
+                    <a>Posts</a>
+                  </ActiveLink>
               </nav>
 
               <SignInButton />
